092a9ceb82290ea599e2b8b799ebfecb2bfa1fdf
Calvin <johncalvinkarume@gmail.com>
2023-06-25 07:15:51 -0700

    Complete Task

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+include stdlib
+
+file_line { 'Disable password authentication.':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Use private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+}
