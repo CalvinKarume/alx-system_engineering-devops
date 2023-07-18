eca921dae01979db61e1463a035c07f594e5d41d
Calvin <johncalvinkarume@gmail.com>
2023-07-18 16:47:29 -0700

    Changes made

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,25 +1,4 @@
-# Installs a Nginx server with custom HTTP header
-
-exec {'update':
-  provider => shell,
-  command  => 'sudo apt-get -y update',
-  before   => Exec['install Nginx'],
-}
-
-exec {'install Nginx':
-  provider => shell,
-  command  => 'sudo apt-get -y install nginx',
-  before   => Exec['add_header'],
-}
-
-exec { 'add_header':
-  provider    => shell,
-  environment => ["HOST=${hostname}"],
-  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
-  before      => Exec['restart Nginx'],
-}
-
-exec { 'restart Nginx':
-  provider => shell,
-  command  => 'sudo service nginx restart',
-}
+[master f1b9ad8] Finally did it
+ 2 files changed, 28 insertions(+), 9 deletions(-)
+ rewrite 0x0F-load_balancer/1-install_load_balancer (100%)
+ create mode 100755 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
