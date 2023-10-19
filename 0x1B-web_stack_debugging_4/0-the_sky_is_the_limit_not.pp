d2891a7ba111bcd80b2a331fe7d9e0c37feabb1d
jahnte <johncalvinkarume@gmail.com>
2023-10-19 14:10:09 -0700

    Completed all tasks

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Address the issue of a substantial volume of requests
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
+
